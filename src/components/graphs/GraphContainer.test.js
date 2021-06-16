b23ca25c221efd109c80df5b9b86569b75e3840e
krsnyder <kirk.robert.snyder@gmail.com>
2021-06-16 11:09:58 -0400

    Setup testing for graph filter

--- src/components/graphs/GraphContainer.test.js
+++ src/components/graphs/GraphContainer.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { render, cleanup } from '../../utils/test-utils';
+import {
+  // Imported render is customer function built in /utils/test-utils
+  render,
+  cleanup,
+  screen,
+  act,
+  fireEvent,
+} from '../../utils/test-utils';
 import GraphContainer from './GraphContainer';
+import userEvent, { specialChars } from '@testing-library/user-event';
+import { expect } from '@jest/globals';
 
 afterEach(() => {
   cleanup();
@@ -11,5 +20,20 @@ describe('<GraphContainer />', () => {
   test('Component renders', async () => {
     const { container } = await render(<GraphContainer />);
     expect(container).toContainElement(container.firstChild);
+  });
+});
+
+describe('Filter by selected state', () => {
+  test('User can choose a state to filter by', async () => {
+    await render(<GraphContainer />);
+    const searchBar = screen.getByRole('combobox');
+    const methodsText = screen.getByText(/person/i);
+    expect(methodsText).not.toBe(null);
+    await act(async () => {
+      userEvent.type(searchBar, `California{enter}{enter}`);
+    });
+
+    const text = screen.getByText(/total number of incidents in california/i);
+    expect(text).toBe(null);
   });
 });
